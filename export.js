fbee0d212b7e0420bd80ecb4292b8bd3b4305804
henryjeff <henryheffernan@gmail.com>
2017-03-28 19:25:57 -0400

    Added multiPartExport capabilities (Export top and bottom halves seperately) along with visibilty formating for exporting groups

--- export.js
+++ export.js
@@ -1,6 +1,5 @@
 //Pixel Art Exporter
 //By Henry Heffernan
-
 var doc = app.activeDocument;
 var docLayers = [];
 var docLayers = getLayers(doc, docLayers);
@@ -8,28 +7,62 @@ var docLayers = getLayers(doc, docLayers);
 //Laptop directory
 var laptopDir = "C:/Storage/Programming/Photoshop Scripts/Test Exports";
 //Desktop directory
-var desktopDir = "D:/Storage/Programming/Photoshop Scripts/Test Exports"
-//exportFrames("test", "C:/Storage/Programming/Photoshop Scripts/Test Exports");
-alert("Doc layers: " + docLayers)
+var desktopDir = "D:/Programming/Photoshop Scripts/Test Exports";
 
-function getLayers (doc, docLayers){
-    for (var m = 0; m < doc.layers.length; m++){
+var varients = ["legs", "arms"];
+multiPartExportFrames(docLayers, "running", varients, desktopDir);
+
+function getLayers(doc, docLayers) {
+    for (var m = 0; m < doc.layers.length; m++) {
         var curLayer = doc.layers[m];
-        if (curLayer.typename == "ArtLayer"){
+        if (curLayer.typename == "ArtLayer") {
             docLayers.push(curLayer);
-        }else{
+        } else {
             getLayers(curLayer, docLayers);
         }
     }
     return docLayers;
 }
 
-function getFrameCount() {
-    var k = 1;
-    while (goToFrame(k) != false) {
-        k++;
+function formatVisibilty(docLayers, animationName, varient) {
+    for (var i = 0; i < docLayers.length; i++) {
+        var docName = "[Document " + doc.name + "]";
+        var prevParent = docLayers[i];
+        var parent = prevParent.parent;
+        goToFrame(1);
+        if (parent == docName) {
+            prevParent.visible = false;
+            continue;
+        }
+        while (parent.parent != docName) {
+            var temp = parent;
+            parent = parent.parent;
+            prevParent = temp;
+        }
+        alert("Parent: " + parent + " | PrevParent: " + prevParent);
+        if (parent == "[LayerSet " + animationName + "]") {
+            parent.visible = true;
+            if (varient != null || varient != undefined || varient != "") {
+                if (prevParent == "[LayerSet " + varient + "]") {
+                    prevParent.visible = true;
+                } else {
+                    prevParent.visible = false;
+                }
+            } else {
+                continue;
+            }
+        } else {
+            parent.visible = false;
+        }
     }
-    return k - 1;
+}
+
+function getFrameCount() {
+    var count = 1;
+    while (goToFrame(count) != false) {
+        count++;
+    }
+    return count - 1;
 }
 
 function goToFrame(jumpToFrame) {
@@ -44,23 +77,20 @@ function goToFrame(jumpToFrame) {
     return false;
 }
 
-
-function multipleExportFrames(animationName, varients, baseDir){
-    for(var i = 0; i < varients.length; i++){
-        // TODO
-        // formatVisibilty(varients[i]);
-        // formatCanvas(varients[i])
+function multiPartExportFrames(docLayers, animationName, varients, baseDir) {
+    for (var i = 0; i < varients.length; i++) {
+        formatVisibilty(docLayers, animationName, varients[i]);
         exportFrames(animationName + "_" + varients[i], baseDir);
     }
 }
 
-function exportFrames(animationName, baseDir){
+function exportFrames(animationName, baseDir) {
     var frameCount = getFrameCount();
     exportFolder = Folder(baseDir + "/" + animationName);
-    if(!exportFolder.exists) exportFolder.create();
-    for(var i = 1; i <= frameCount; i++){
+    if (!exportFolder.exists) exportFolder.create();
+    for (var i = 1; i <= frameCount; i++) {
         goToFrame(i);
-        var pngFile =   File(baseDir + "/" + animationName + "/" + animationName + "_" + i + ".png");
+        var pngFile = File(baseDir + "/" + animationName + "/" + animationName + "_" + i + ".png");
         doc.saveAs(pngFile, new PNGSaveOptions(), true, Extension.LOWERCASE);
     }
 }
